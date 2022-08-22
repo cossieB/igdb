e8d7bd7f1963fc4dae172694a5affab416266409
Buntu Cossie <91845390+cossieB@users.noreply.github.com>
2022-08-22 14:35:11 +0200

    chore: removed mongoose models

--- models/actor.ts
+++
@@ -1,6 +0,0 @@
-export interface IActor {
-    id?: string,
-    name: string,
-    photo?: string,
-    summary?: string
-}
--- models/platform.ts
+++
@@ -1,7 +0,0 @@
-export interface IPlatform {
-    id?: string,
-    name: string,
-    logo: string,
-    release: Date | string,
-    summary: string,
-}

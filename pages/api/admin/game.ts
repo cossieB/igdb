5da706abf0066e99b06c6769e506d25ef7533110
Buntu Cossie <91845390+cossieB@users.noreply.github.com>
2022-08-25 15:42:20 +0200

    refactor: refactored delete method in api/games

--- pages/api/admin/game.ts
+++ pages/api/admin/game.ts
@@ -4,11 +4,11 @@ import { db } from "../../../prisma/db";
 
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
-    
+
     if (req.method == "POST") {
-        
-        const { platformIds} = req.body
-        const game = {...req.body}
+
+        const { platformIds } = req.body
+        const game = { ...req.body }
         delete game.platformIds
         delete game.gameId
         const onPlatform = platformIds.map((platformId: string) => ({ platformId }))
@@ -23,21 +23,21 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
                 },
             })
 
-            return res.json({ msg: "Successfully created " + result.gameId })
+            return res.status(201).json({ msg: "Successfully created " + result.gameId })
         }
         catch (e: any) {
             console.error(e);
-            return res.json({error: e.message})
+            return res.json({ error: e.message })
         }
     }
-    if (req.method = "PUT") {
+    if (req.method == "PUT") {
         try {
-            const { platformIds} = req.body
-            const game = {...req.body}
+            const { platformIds } = req.body
+            const game = { ...req.body }
             delete game.platformIds
             delete game.gameId
             const onPlatform: GamesOnPlatforms[] = platformIds.map((platformId: string) => ({ platformId, gameId: req.body.gameId }))
-            
+
             const prom1 = db.game.update({
                 where: {
                     gameId: req.body.gameId
@@ -55,12 +55,29 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
                 data: onPlatform
             })
             const result = await db.$transaction([prom1, prom2, prom3])
-            
+
             return res.json({ msg: "Successfully updated " + result[0].gameId })
-        } 
+        }
         catch (e: any) {
             console.error(e);
-            return res.json({error: e.message})
+            return res.json({ error: e.message })
         }
     }
+    if (req.method == "DELETE") {
+        
+        try {
+            const {gameId} = req.body as { gameId: string }
+            await db.game.delete({
+                where: {
+                    gameId
+                }
+            })
+            return res.json({msg: "Successfully deleted " + gameId})
+        }
+        catch (e: any) {
+            console.error(e)
+            return res.json({ error: e.message })
+        }
+    }
+    return res.status(501).json({error: "Invalid method"})
 }

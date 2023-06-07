7123bcec1160dd305319417556f4304fe2671ee4
Buntu Cossie <91845390+cossieB@users.noreply.github.com>
2023-06-07 19:04:22 +0200

    fix: graphql cors

--- pages/api/graphql/index.ts
+++ pages/api/graphql/index.ts
@@ -1,22 +1,18 @@
 import { ApolloServer } from '@apollo/server';
 import { startServerAndCreateNextHandler } from '@as-integrations/next';
-import { ApolloServerPluginLandingPageLocalDefault, ApolloServerPluginLandingPageProductionDefault } from "@apollo/server/plugin/landingPage/default"
+import { ApolloServerPluginLandingPageProductionDefault } from "@apollo/server/plugin/landingPage/default"
 import { resolvers } from '../../../graphql/resolvers';
 import { typeDefs } from '../../../graphql/typeDefs';
+import allowCors from '../../../utils/cors';
 
 const server = new ApolloServer({
     resolvers,
     typeDefs,
-    plugins: [
-        process.env.NODE_ENV! == 'development'
-            ?
-            ApolloServerPluginLandingPageLocalDefault()
-            :
-            ApolloServerPluginLandingPageProductionDefault({
-                embed: true,
-                graphRef: "cossieb@main"
-            })
-    ]
+    plugins: [ApolloServerPluginLandingPageProductionDefault()]
 });
 
-export default startServerAndCreateNextHandler(server);
+const handler = startServerAndCreateNextHandler(server, {
+    context: async (req, res) => ({req, res})
+});
+
+export default allowCors(handler)

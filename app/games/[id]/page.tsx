0ad36728bf8abb8a8a7f22ad2b0498b094883dfe
Buntu Cossie <91845390+cossieB@users.noreply.github.com>
2024-04-14 17:55:33 +0200

    added actors to games game

--- app/games/[id]/page.tsx
+++ app/games/[id]/page.tsx
@@ -1,12 +1,9 @@
-import { Developer, Game, Platform, Publisher } from '@prisma/client'
 import { Metadata } from 'next'
 import Description from '../../../components/Description'
 import DevTile from '../../../components/DevTile'
 import Tags from '../../../components/Tags'
-import { Optional } from '../../../lib/utilityTypes'
 import { db } from '../../../prisma/db'
 import styles from '../../../styles/Games.module.scss'
-import { joinQuery } from '../../../utils/JoinResult'
 import { notFound } from 'next/navigation'
 import YouTubeIframe from '../../../components/YouTubeIframe'
 import Header from '../../../components/Header'
@@ -27,7 +24,7 @@ export async function generateMetadata({ params }: Props): Promise<Metadata> {
 }
 
 export default async function GameId({ params }: Props) {
-    const { game, genres } = await getData({ params });
+    const { game } = await getData({ params });
 
     return (
         <div className='container'>
@@ -38,7 +35,7 @@ export default async function GameId({ params }: Props) {
                 </div>
             </div>
 
-                <Header heading='Info' />
+            <Header heading='Info' />
             <div className={styles.main} >
                 <div className={styles.infobar}>
                     <div className={styles.date} >
@@ -47,10 +44,26 @@ export default async function GameId({ params }: Props) {
                         <div> {new Date(game.releaseDate).toLocaleString('en-za', { month: 'short' })} </div>
                         <div> {new Date(game.releaseDate).getFullYear()} </div>
                     </div>
-                    <Tags isLink={true} tags={genres} />
+                    <Tags isLink={true} tags={game.GenresOfGames.map(item => item.genre)} />
                 </div>
                 <Description className={styles.description} html={game.summary} />
             </div>
+            {game.ActorsInGames.length > 0 && (
+                <>
+                    <Header heading='Cast' />
+                    <div className={styles.cast}>
+                        {game.ActorsInGames.map(item =>
+                            <div className={styles.actorCard} key={item.actorId}>
+                                <img src={item.actor.photo ?? "https://upload.wikimedia.org/wikipedia/commons/5/55/Question_Mark.svg"} alt={item.actor.name} />
+                                <div>
+                                    <span>{item.actor.name}</span> <br />
+                                    <span className={styles.char} >{item.character}</span>
+                                </div>
+                            </div>
+                        )}
+                    </div>
+                </>
+            )}
             <Header heading='Trailer' />
             <div className={styles.video} >
                 <YouTubeIframe link={game.trailer} />
@@ -68,7 +81,7 @@ export default async function GameId({ params }: Props) {
             </div>
             <Header heading='Platforms' />
             <div className={styles.platforms} >
-                {game.platforms.map(item => <DevTile key={item.platformId} item={{ ...item, id: item.platformId }} href="platform" className={styles.logoTile} />)}
+                {game.GamesOnPlatforms.map(item => <DevTile key={item.platformId} item={{ ...item.platform, id: item.platformId }} href="platform" className={styles.logoTile} />)}
             </div>
         </div>
     )
@@ -80,66 +93,36 @@ async function getData({ params }: Props) {
     if (id.length != 36)
         return notFound()
 
-    const queryRes = await joinQuery(id)
+    const game = await db.game.findUnique({
+        where: {
+            gameId: params.id
+        },
+        include: {
+            developer: true,
+            publisher: true,
+            ActorsInGames: {
+                include: {
+                    actor: true
+                },
+                orderBy: {
+                    importance: 'asc'
+                }
+            },
+            GamesOnPlatforms: {
+                include: {
+                    platform: true
+                }
+            },
+            GenresOfGames: true,
+        }
+    })
 
-    if (queryRes.length == 0)
+    if (!game)
         return notFound()
 
-    const platforms: Pick<Platform, 'platformId' | 'name' | 'logo'>[] = await db.$queryRaw`
-        SELECT 
-            "platformId", 
-            "Platform".name , 
-            "Platform".logo
-        FROM 
-            "Platform"
-        INNER JOIN 
-            "GamesOnPlatforms" USING ("platformId")
-        INNER JOIN 
-            "Game" USING ("gameId")
-        WHERE "gameId" = ${id}::UUID;
-        `
-    type GameResult = (Game & {
-        developer: Optional<Developer, 'country' | 'location' | 'summary'>;
-        publisher: Optional<Publisher, 'country' | 'headquarters' | 'summary'>;
-        platforms: Pick<Platform, 'platformId' | 'name' | 'logo'>[]
-    })
-
-    const game: GameResult = {
-        banner: queryRes[0].banner,
-        title: queryRes[0].title,
-        cover: queryRes[0].cover,
-        developerId: queryRes[0].developerId,
-        publisherId: queryRes[0].publisherId,
-        gameId: queryRes[0].gameId,
-        images: queryRes[0].images,
-        releaseDate: queryRes[0].releaseDate,
-        summary: queryRes[0].summary,
-        trailer: queryRes[0].trailer,
-        platforms,
-        developer: {
-            developerId: queryRes[0].developerId,
-            name: queryRes[0].dev_name,
-            logo: queryRes[0].dev_logo,
-        },
-        publisher: {
-            publisherId: queryRes[0].publisherId,
-            name: queryRes[0].dev_name,
-            logo: queryRes[0].pub_logo,
-        }
-    }
-    const genresQuery = await db.genresOfGames.findMany({
-        where: {
-            gameId: game.gameId
-        },
-        select: {
-            genre: true
-        }
-    })
-    const genres = genresQuery.map(g => g.genre)
 
     return {
         game,
-        genres
     }
 }
 export async function generateStaticParams() {
